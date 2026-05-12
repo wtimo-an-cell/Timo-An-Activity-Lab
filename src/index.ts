2ace72f3f87bed745b12aaf44ba0a253a72be588
wtimo-an-cell <wtimo-an@aclcbukidnon.com>
2026-05-12 14:10:44 +0800

    Delete src directory

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from './app.js';
-
-const PORT = 3000;
-const app = createApp();
-
-app.listen(PORT, () => {
-  console.log(`\n🚀 API Server ready at http://localhost:${PORT}`);
-  console.log(`🚀 Web Client ready at http://localhost:5173\n`);
-});
